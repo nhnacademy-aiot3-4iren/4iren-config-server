dc2ec97a898fda155f0cb492c01d36b72d24ec4f
조선대-NHN 29 <chosun-nhn29@joseondae-NHNui-noteubug-2.local>
2026-07-23 09:55:23 +0900

    fix: test 오류 수정

--- src/test/java/com/nhnacademy/config/ConfigApplicationTests.java
+++ src/test/java/com/nhnacademy/config/ConfigApplicationTests.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.config;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,6 +22,7 @@ class ConfigApplicationTests {
      * encrypt.key 값이 정상적으로 주입되는지 확인하는 테스트 메서드
      */
     @Test
+    @DisplayName("encrypt.key 값 확인")
     void check_encrypt_key() {
         System.out.println("=======================================");
         System.out.println("encrypt.key: '"+encryptKey+"'");
@@ -33,6 +35,7 @@ class ConfigApplicationTests {
      * {cipher} 접두사는 암호화된 문자열임을 표시해주는 용도로, yml이나 properties 파일에 암호화된 문자열을 넣을 때 사용
      */
     @Test
+    @DisplayName("암호화 테스트")
     void encryptor_encode_test() {
         String originalText="my-secret-password";
 
@@ -48,8 +51,9 @@ class ConfigApplicationTests {
      * 앞에 붙은 {cipher}는 암호화된 문자열임을 표시해주는 접두사로, 복호화 시에는 제거하고 사용
      */
     @Test
+    @DisplayName("복호화 테스트")
     void encryptor_decode_test() {
-        String encryptedText="my-secret-password";
+        String encryptedText="a9fec3f6402cbdbccac4ebb388645777e0868f070cc049489e7843c3ad2ded0ee2db51a209238af2e9b912da953664e1";
 
         String decryptedText= textEncryptor.decrypt(encryptedText);
 
